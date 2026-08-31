ec4eca3c9fece7b447688f07bc61d4bc56a733d6
walidkarouach <karouachwalid700@gmail.com>
2026-08-31 20:13:01 +0100

    Ajouter route user

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
 Route::post('/register', [AuthController::class, 'register']);
+
 Route::post('/login', [AuthController::class, 'login']);
+
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
